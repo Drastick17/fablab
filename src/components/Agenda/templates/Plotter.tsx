4e9aae5b668596d97c3988505d4b482efeb04149
ArielCedillo225 <arielcedillo225@gmail.com>
2024-03-24 12:05:25 -0500

    Button and Styles Agenda Services

--- src/components/Agenda/templates/Plotter.tsx
+++ src/components/Agenda/templates/Plotter.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete } from "@mui/material";
+import { Autocomplete, Button, Typography } from "@mui/material";
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -55,32 +55,72 @@ export default function Plotter() {
   return (
     <Box
       sx={{
-        width: "100vw",
-        height: "100vh",
+        width: "100%",
+        height: "100%",
         typography: "body1",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
       }}
     >
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={data}
-        onChange={(evt, values) => {
-          setSelectedMaterial(values);
+      <Box
+        sx={{
+          width: "100%",
+          height: "55px",
+          display: "flex",
+          alignContent: "center",
+          justifyContent: "center",
+          gap: "20px",
         }}
-        sx={{ width: 300 }}
-        renderInput={(params) => (
-          <TextField {...params} label="Material de impresión" />
-        )}
-      />
+      >
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={data}
+          onChange={(evt, values) => {
+            setSelectedMaterial(values);
+          }}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Material de impresión" />
+          )}
+        />
 
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={calidad}
-        disabled={Object.keys(selectedMaterial).length == 0}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Dato 2" />}
-      />
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={calidad}
+          disabled={Object.keys(selectedMaterial).length == 0}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Calidad de impresión" />
+          )}
+        />
+        <Button variant="contained" color="error">
+          Agendar
+        </Button>
+      </Box>
+
+      <Box
+        sx={{
+          width: "85%",
+          height: "200px",
+          border: "0.5px solid",
+          borderRadius: "5px",
+          margin: "100px 100px 0px 100px",
+        }}
+      >
+        <Typography
+          sx={{ padding: "20px", fontSize: "0.7rem", color: "red" }}
+          variant="body2"
+        >
+          *El agendamiento de las citas se dará de acuerdo a la disponibilidad
+          de FabLab, recuerda que los horarios de atención son de 08:00 a 13:00
+          y de 14:00 a 17:00. No te olvides de revisar constantemente tu correo
+          electrónico, enviaremos mensajes de confirmación y agendación cuando
+          se procese tu solicitud.*
+        </Typography>
+      </Box>
     </Box>
   );
 }
